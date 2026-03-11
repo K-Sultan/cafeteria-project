281d04b3dff4d5be8c873eb13a2724cd17aaf8b5
Arsany Ashraf <arsanyarsany4800@gmail.com>
2026-03-11 20:59:22 +0200

    Add user management routes for adding and listing users

--- index.php
+++ index.php
@@ -7,11 +7,16 @@ require_once "app/controllers/AuthController.php";
 
 $router = new Router();
 
-$router->get("/", [UserController::class, "index"]);
-$router->post("/user", [UserController::class, "home"]);
+//TODO : the / should be handled depending on the authorization(user or admin)
+// $router->get("/", [UserController::class, "index"]);
+// $router->post("/user", [UserController::class, "home"]);
+
 $router->get("/login", [AuthController::class, "index"]);
 $router->post("/login", [AuthController::class, "login"]);
 $router->get("/forgotpassword", [AuthController::class, "forgotIndex"]);
 $router->post("/forgotpassword", [AuthController::class, "forgotPassword"]);
+$router->get("/users/add", [UserController::class, "add"]);
+$router->post("/users", [UserController::class, "store"]);
+$router->get("/users", [UserController::class, "index"]);
 
 $router->resolve();
